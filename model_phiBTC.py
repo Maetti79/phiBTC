c5e5df52855d3c10873db40a184515db0328d160
Dennis Mittmann <dennis@compucampus.de>
2025-04-01 22:42:44 +0200

    Update model_phiBTC.py

--- model_phiBTC.py
+++ model_phiBTC.py
@@ -1,1 +1,39 @@
 # Core formula implementation placeholder
+# model_phiBTC.py
+
+import numpy as np
+
+class PhiBTCModel:
+    def __init__(self, weights=None, lambda_=0.3, P0=3000):
+        # Default weights for each variable
+        self.weights = weights or {
+            'hashrate': 1.5,
+            'network_usage': 1.2,
+            'macro_stress': 0.8,
+            'sentiment': 0.5,
+            'liquidity': 1.0,
+            'regulation': 1.0,
+            'halving': 0.9
+        }
+        self.lambda_ = lambda_
+        self.P0 = P0
+
+    def compute_phi(self, data):
+        """
+        data: dict with keys - hashrate, network_usage, macro_stress,
+              sentiment, liquidity, regulation, halving
+        Each value should be a float or numpy array of equal length.
+        """
+        phi = (
+            self.weights['hashrate'] * np.log(data['hashrate']) +
+            self.weights['network_usage'] * np.log(data['network_usage']) +
+            self.weights['macro_stress'] * data['macro_stress'] +
+            self.weights['sentiment'] * data['sentiment'] +
+            self.weights['liquidity'] * np.log(data['liquidity']) +
+            self.weights['regulation'] * data['regulation'] +
+            self.weights['halving'] * data['halving']
+        )
+        return phi
+
+    def predict_price(self, phi):
+        return self.P0 * np.exp(self.lambda_ * phi)
