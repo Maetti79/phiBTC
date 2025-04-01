c76f489535df3c40133a6f2e0b21ec6b048ab997
Dennis Mittmann <dennis@compucampus.de>
2025-04-01 22:43:22 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,1 +1,45 @@
 # Real-time API calls placeholder
+# fetch_data.py
+
+import requests
+import pandas as pd
+from datetime import datetime
+
+class DataFetcher:
+    def __init__(self):
+        self.cg_url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart'
+        self.bc_hashrate_url = 'https://api.blockchain.info/charts/hash-rate?timespan=1days&format=json'
+
+    def fetch_price_volume(self):
+        params = {'vs_currency': 'usd', 'days': '1', 'interval': 'daily'}
+        response = requests.get(self.cg_url, params=params)
+        data = response.json()
+        price = data['prices'][-1][1]
+        volume = data['total_volumes'][-1][1]
+        return price, volume
+
+    def fetch_hashrate(self):
+        response = requests.get(self.bc_hashrate_url)
+        data = response.json()
+        hashrate = data['values'][-1]['y']
+        return hashrate
+
+    def get_latest_inputs(self):
+        price, volume = self.fetch_price_volume()
+        hashrate = self.fetch_hashrate()
+
+        # Placeholder values for other metrics (mock until API available)
+        return {
+            'hashrate': hashrate,
+            'network_usage': 1e5,     # placeholder
+            'macro_stress': 1.0,      # neutral macro index
+            'sentiment': 0.5,         # moderately positive
+            'liquidity': volume,
+            'regulation': 1.0,        # neutral regulation
+            'halving': 0.3            # time-proximity weight to halving
+        }
+
+if __name__ == '__main__':
+    fetcher = DataFetcher()
+    inputs = fetcher.get_latest_inputs()
+    print("Live inputs:", inputs)
